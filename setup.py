d9491e724ddabfce30d959c40d4f2ef352568600
Mark van de Korput <dr.theman@gmail.com>
2016-09-13 02:10:25 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='evento',
+      version='0.0.1',
+      description='Observer pattern made muy fácil',
+      url='http://github.com/markkorput/evento',
+      author='Mark van de Korput',
+      author_email='dr.theman@gmail.com',
+      license='MIT',
+      packages=['evento'],
+      zip_safe=True)
