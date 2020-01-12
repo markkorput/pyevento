9c01111179d28a3e4b50471f2e9e37b3e39bef80
mark <mark.korput@gmail.com>
2020-01-12 13:18:10 +0100

    bump to 1.0.2, add long description

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup
+import os.path
+
+long_description = ''
+with open(path.join(os.path.dirname(__name__), 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(name='evento',
-      version='1.0.1',
+      version='1.0.2',
       description='Observer pattern made muy facil',
+      long_description=long_description,
+      long_description_content_type='text/markdown'
       url='http://github.com/markkorput/pyevento',
       author='Mark van de Korput',
       author_email='dr.theman@gmail.com',
