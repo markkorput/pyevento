c260cb26ad7f2bd9d597cd4745d3e04c7d353836
mark <mark.korput@gmail.com>
2020-01-12 13:20:06 +0100

    typo

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 import os.path
 
 long_description = ''
-with open(path.join(os.path.dirname(__name__), 'README.md'), encoding='utf-8') as f:
+with open(os.path.join(os.path.dirname(__name__), 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='evento',
       version='1.0.2',
       description='Observer pattern made muy facil',
       long_description=long_description,
-      long_description_content_type='text/markdown'
+      long_description_content_type='text/markdown',
       url='http://github.com/markkorput/pyevento',
       author='Mark van de Korput',
       author_email='dr.theman@gmail.com',
